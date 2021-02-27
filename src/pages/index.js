0e8cbc93daa01b93c883d2404c86fcf4149d70f4
Varun <varungarg455@gmail.com>
2021-02-27 19:32:29 +0530

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>I am Varun working at Sapient, living in New Delhi.</h2>
+    </div>
+  )
 }
+
+export default IndexPage;
